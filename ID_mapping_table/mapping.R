69869a213256eae2991244ee2c0a0ec401eb3bef
fff0996 <54706837+fff0996@users.noreply.github.com>
2021-01-29 01:50:37 +0900

    Update mapping.R

--- ID_mapping_table/mapping.R
+++ ID_mapping_table/mapping.R
@@ -1,28 +1,39 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
+#If you don't install "biomaRt"
+#########################################################
+#if (!requireNamespace("BiocManager", quietly = TRUE))
   
-  install.packages("BiocManager")
-BiocManager::install(version = "3.12")
-BiocManager::install("biomaRt")
+  #install.packages("BiocManager")
+#BiocManager::install(version = "3.12")
+#BiocManager::install("biomaRt")
+#########################################################
 
+#Loading biomaRt library
 library("biomaRt")
 
 
-listEnsembl()
+#make database
+#use Homosapiens gene
+#mapping to gene_ID (Entrez)
 ensembl <- useEnsembl(biomart="genes")
 datasets <- listDatasets(ensembl)
 ensembl <-useDataset(dataset="hsapiens_gene_ensembl",mart=ensembl)
 attributes <- listAttributes(ensembl)
 
+#Get targets to map
 target <- read.table("target.txt")
 uniprot <- target[,1]
-
+#make mapping table
+#fill out target type
 mapping <- getBM(filters="uniprotswissprot",attributes = c("uniprotswissprot","ensembl_peptide_id"),
                  values=uniprot,mart=ensembl)
+#make output file
 write.table(mapping,"C:/Users/fff09/Documents/Uniprotswissprot_to_entrezgene_id.txt",row.names=FALSE,sep="\t")
-
+#Get target to map
 ENSP <- read.table("ENSP.txt")
 ENSP <- ENSP[,1]
-head(ENSP)
+#make mapping table
+#fill out target type
 ENSP_to_ENTREZ <- getBM(filters="ensembl_peptide_id",attributes = c("ensembl_peptide_id","entrezgene_id"),
                         values=ENSP,mart=ensembl)
+#make output file
 write.table(ENSP_to_ENTREZ,"C:/Users/fff09/Documents/ENSP_to_ENTREZ.txt",row.names=FALSE,sep="\t")
