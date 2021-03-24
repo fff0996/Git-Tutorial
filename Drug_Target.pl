f5d1a2a7b53433dd1e06ff9af2dd200aaa1a43db
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2021-03-25 03:50:58 +0900

    Update Drug_Target.pl

--- Drug_Target.pl
+++ Drug_Target.pl
@@ -1,36 +1,20 @@
-#!/usr/bin/perl
-
-use warnings;
-use strict;
-
-my $file=$ARGV[0];
-my %hash;
-
-open(I,$file);
-
-my $i=<I>;
-chomp($i);
-
-while(<I>){
-   
-   $i=$_;
-   chomp($i);
-   my ($Drug,$Target)= split(/\t/,$i);
-   if(exists $hash{$Drug}){
-       push(@{$hash{$Drug}},$Target);   
+#making hash table 
+# \hash table, Drug, target
+sub Drug_Target{
+   my($hash,$Drug,$Target)=@_;
+   if(exists %$hash{$Drug}{
+      push(@{%$hash{$Drug}},$Target);
    }else{
-     @{$hash{$Drug}}=();
-     push(@{$hash{$Drug}},$Target);
-    }      
+      @{%$hash{$Drug}}=();
+      push(@{%$hash{$Drug}},$Target);
+   }
 }
-close I;
 
-for my $i (keys %hash){
-        print $i," ";
-         for my $j (@{$hash{$i}}){
-             print $j," ";
-         }
-        print "\n";
-}
+   
+
+
+
+
+
 
 
