d296f47a17eb3138290c1c3950254c5522fabf7a
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2021-03-30 17:41:58 +0900

    Update mapping.pm

--- mapping.pm
+++ mapping.pm
@@ -2,7 +2,7 @@
 # \hash table, Drug, target
 sub Drug_Target{
    my($hash,$Drug,$Target)=@_;
-   if(exists %$hash{$Drug}{
+   if(exists %$hash{$Drug}){
       push(@{%$hash{$Drug}},$Target);
    }else{
       @{%$hash{$Drug}}=();
